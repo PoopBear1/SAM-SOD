3590392f39cbf264fb6c34ae1bba09e064c60b8e
Ruikai Cui <ruikai.cui@anu.edu.au>
2023-06-02 16:57:45 +1000

    add adalora

--- methods/adalora.py
+++ methods/adalora.py
@@ -5,46 +5,31 @@ from torch.nn.parameter import Parameter
 from segment_anything.modeling import Sam
 from safetensors import safe_open
 from methods.segment_anything import sam_model_registry
+import loralib
 
 custom_config = {'base'      : {'strategy': 'base_adam',
                                 'batch': 2,
                                },
                  'customized': {'--ckpt_path': {'type': str, 'default': './ckpts/sam_vit_b_01ec64.pth'},
                                 '--model_type': {'type': str, 'default': 'vit_b'},
-                                '--rank': {'type': int, 'default': 4},
+                                '--rank': {'type': int, 'default': 12},
+                                '--target_rank': {'type': int, 'default': 8},
                                },
                 }
 
 
-class _LoRA_qkv(nn.Module):
-    """In Sam it is implemented as
-    self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
-    B, N, C = x.shape
-    qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, self.head_dim).permute(2, 0, 3, 1, 4)
-    q, k, v = qkv.unbind(0)
-    """
-
-    def __init__(
-            self,
-            qkv: nn.Module,
-            linear_a_q: nn.Module,
-            linear_b_q: nn.Module,
-            linear_a_v: nn.Module,
-            linear_b_v: nn.Module,
-    ):
+class _AdaLoRA_qkv(nn.Module):
+    def __init__(self, qkv, linear_q, linear_v):
         super().__init__()
         self.qkv = qkv
-        self.linear_a_q = linear_a_q
-        self.linear_b_q = linear_b_q
-        self.linear_a_v = linear_a_v
-        self.linear_b_v = linear_b_v
+        self.linear_q = linear_q
+        self.linear_v = linear_v
         self.dim = qkv.in_features
-        self.w_identity = torch.eye(qkv.in_features)
 
     def forward(self, x):
         qkv = self.qkv(x)  # B,N,N,3*org_C
-        new_q = self.linear_b_q(self.linear_a_q(x))
-        new_v = self.linear_b_v(self.linear_a_v(x))
+        new_q = self.linear_q(x)
+        new_v = self.linear_v(x)
         qkv[:, :, :, : self.dim] += new_q
         qkv[:, :, :, -self.dim:] += new_v
         return qkv
@@ -65,8 +50,6 @@ class Network(nn.Module):
         # dim = base_vit_dim
         self.lora_layer = list(range(len(sam_model.image_encoder.blocks)))  # Only apply lora to the image encoder by default
         # create for storage, then we can init them or load weights
-        self.w_As = []  # These are linear layers
-        self.w_Bs = []
 
         # lets freeze first
         for param in sam_model.image_encoder.parameters():
@@ -79,29 +62,16 @@ class Network(nn.Module):
                 continue
             w_qkv_linear = blk.attn.qkv
             self.dim = w_qkv_linear.in_features
-            w_a_linear_q = nn.Linear(self.dim, r, bias=False)
-            w_b_linear_q = nn.Linear(r, self.dim, bias=False)
-            w_a_linear_v = nn.Linear(self.dim, r, bias=False)
-            w_b_linear_v = nn.Linear(r, self.dim, bias=False)
-            self.w_As.append(w_a_linear_q)
-            self.w_Bs.append(w_b_linear_q)
-            self.w_As.append(w_a_linear_v)
-            self.w_Bs.append(w_b_linear_v)
-            blk.attn.qkv = _LoRA_qkv(
+            linear_q = loralib.SVDLinear(in_features=self.dim, out_features=self.dim, r=r, bias=False)
+            linear_v = loralib.SVDLinear(in_features=self.dim, out_features=self.dim, r=r, bias=False)
+            blk.attn.qkv = _AdaLoRA_qkv(
                 w_qkv_linear,
-                w_a_linear_q,
-                w_b_linear_q,
-                w_a_linear_v,
-                w_b_linear_v,
+                linear_q,
+                linear_v
             )
-        self.reset_parameters()
         self.sam = sam_model
+        # loralib.mark_only_lora_as_trainable(self.sam)
 
-    def reset_parameters(self) -> None:
-        for w_A in self.w_As:
-            nn.init.kaiming_uniform_(w_A.weight, a=math.sqrt(5))
-        for w_B in self.w_Bs:
-            nn.init.zeros_(w_B.weight)
 
     def forward(self, x, phase='test'):
         batched_input = x
