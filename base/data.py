6c4e5042059fbf001665102f3f58a1dfe939891f
Ruikai Cui@cluster1.cecs.anu.edu.au <ruikai.cui@anu.edu.au>
2023-06-02 15:52:52 +1000

    change dataloader

--- base/data.py
+++ base/data.py
@@ -14,7 +14,7 @@ def get_image_list(name, config, phase):
     gts = []
     if name in ('simple', 'tough', 'normal'):
         train_split = 10000
-        
+
         print('Objectness shifting experiment.')
         # Objectness
         list_file = 'clean_list.txt'
@@ -25,29 +25,29 @@ def get_image_list(name, config, phase):
             img_list = f.readlines()[train_split:-train_split]
         else:
             img_list = f.readlines()[:train_split]
-                
+
         for i in range(len(img_list)):
             img_list[i] = img_list[i].split(' ')[0]
-                
+
         images = [os.path.join(config['data_path'], 'SALOD/images', line.strip() + '.jpg') for line in img_list]
         gts = [os.path.join(config['data_path'], 'SALOD/mask', line.strip() + '.png') for line in img_list]
-        
+
     # Benchmark
     elif name == 'SALOD':
         f = open(os.path.join(config['data_path'], 'SALOD/{}.txt'.format(phase)), 'r')
         img_list = f.readlines()
-        
+
         images = [os.path.join(config['data_path'], name, 'images', line.strip() + '.jpg') for line in img_list]
         gts = [os.path.join(config['data_path'], name, 'mask', line.strip() + '.png') for line in img_list]
     # Original SOD datasets
     else:
         image_root = os.path.join(config['data_path'], name, 'images')
         gt_root = os.path.join(config['data_path'], name, 'segmentations')
-        
+
         images = sorted([os.path.join(image_root, f) for f in os.listdir(image_root) if f.endswith('.jpg') or f.endswith('.png')])
         gts = sorted([os.path.join(gt_root, f) for f in os.listdir(gt_root) if f.endswith('.png')])
         #print(os.path.join(config['data_path'], name, 'images'), len(images), len(gts))
-    
+
     return images, gts
 
 def get_loader(config):
@@ -93,26 +93,26 @@ class Train_Dataset(data.Dataset):
     def __getitem__(self, index):
         image = Image.open(self.images[index]).convert('RGB')
         gt = Image.open(self.gts[index]).convert('L')
-        
+
         #print('orig: ', image.size, gt.size)
         if self.config['data_aug']:
             #image, gt = rotate(image, gt)
             #image = random_light(image)
             image, gt = RandomCrop(image, gt)
         #print('croped: ', image.size, gt.size)
-        
+
         img_size = self.config['size']
         image = image.resize((img_size, img_size))
         gt = gt.resize((img_size, img_size))
-    
+
         image = np.array(image).astype(np.float32)
         gt = np.array(gt)
-        
+
         #print(image.shape, gt.shape)
         if random.random() > 0.5:
             image = image[:, ::-1]
             gt = gt[:, ::-1]
-        
+
         image = ((image / 255.) - mean) / std
         image = image.transpose((2, 0, 1))
         gt = np.expand_dims((gt > 128).astype(np.float32), axis=0)
@@ -137,9 +137,11 @@ class Test_Dataset:
         #gt = gt.resize((self.config['size'], self.config['size']))
         #gt = np.array(gt).astype(np.float32)
         name = self.images[index].split('/')[-1].split('.')[0]
-        
-        
-        image = ((image / 255.) - mean) / std
+
+
+        # image = ((image / 255.) - mean) / std
+        image = image / 255.
+
         image = image.transpose((2, 0, 1))
         image = torch.tensor(np.expand_dims(image, 0)).float()
         gt = (gt > 128).astype(np.float32)
@@ -148,12 +150,12 @@ class Test_Dataset:
 def test_data():
     config = {'orig_size': True, 'size': 288, 'data_path': '../dataset'}
     dataset = 'SOD'
-    
+
     '''
     data_loader = Test_Dataset(dataset, config)
     #data_loader = Train_Dataset(dataset, config)
     data_size = data_loader.size
-    
+
     for i in range(data_size):
         img, gt, name = data_loader.load_data(i)
         #img, gt = data_loader.__getitem__(i)
@@ -165,13 +167,13 @@ def test_data():
         im = Image.fromarray(new_img)
         #im.save('temp/' + name + '.jpg')
         im.save('temp/' + str(i) + '.jpg')
-    
+
     '''
-    
+
     data_loader = Val_Dataset(dataset, config)
     imgs, gts, names = data_loader.load_all_data()
     print(imgs.shape, gts.shape, len(names))
-    
+
 
 if __name__ == "__main__":
     test_data()
